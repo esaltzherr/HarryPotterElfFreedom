334d10b8cd082233a114c66e36a0882d6c1738c7
Elroy Saltzherr <lsaltzherr@gmail.com>
2023-12-13 15:58:46 -0800

    game resetting set up

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
-const gravity = 0.3;
-const friction = 0.7;
+const gravity = 0.1;
+const friction = 0.5;
 
 // Player properties
 const player = {
@@ -11,43 +11,18 @@ const player = {
     width: 25,
     height: 25,
     color: 'blue',
-    speed: 2.5,
+    speed: 2,
     velocityX: 0,
     velocityY: 0,
+    jumpStrength: 5,
     jumping: false,
+
 };
 
-// Platforms properties
-const platforms = [
-    // Ground floor
-    { x: 0, y: 550, width: canvas.width, height: 20, color: 'saddlebrown' },
-    // Second floor
-    { x: 0, y: 350, width: 500, height: 20, color: 'grey' },
-    { x: 800, y: 350, width: 400, height: 20, color: 'grey' },
-    // Third floor
-    { x: 0, y: 150, width: 400, height: 20, color: 'grey' },
-    { x: 900, y: 150, width: 300, height: 20, color: 'grey' },
-    // Stairs
-    { x: 550, y: 450, width: 200, height: 20, color: 'peru' },
-    { x: 400, y: 250, width: 200, height: 20, color: 'peru' },
-    { x: 700, y: 250, width: 200, height: 20, color: 'peru' },
-    // Invisible barriers
-    { x: -10, y: -50, width: 10, height: canvas.height + 50, color: '' },
-    { x: canvas.width, y: -50, width: 10, height: canvas.height + 50, color: '' },
-];
-
-// Collectibles properties
-const collectibles = [
-    { x: 150, y: 320, width: 10, height: 10, color: 'gold', collected: false },
-    // Add more collectibles as needed
-];
-
-// Enemies properties
-const enemies = [
-    { x: 200, y: 520, width: 30, height: 30, color: 'red', velocityX: 1, visionRange: 100 },
-    { x: 200, y: 200, width: 30, height: 30, color: 'red', velocityX: 1, visionRange: 100 },
-   
-];
+let platforms = [];
+let collectibles = [];
+let enemies = [];
+let currentLevel = 1;
 
 function drawPlayer() {
     ctx.fillStyle = player.color;
@@ -70,33 +45,156 @@ function drawEnemy(enemy) {
     ctx.fillStyle = enemy.color;
     ctx.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
 
-    // Drawing vision cone
     ctx.beginPath();
     ctx.moveTo(enemy.x + enemy.width / 2, enemy.y + enemy.height / 2);
-
-    if (enemy.velocityX > 0) { // Enemy moving right
+    if (enemy.velocityX > 0) {
         ctx.lineTo(enemy.x + enemy.width / 2 + enemy.visionRange, enemy.y - enemy.visionRange);
         ctx.lineTo(enemy.x + enemy.width / 2 + enemy.visionRange, enemy.y + enemy.height + enemy.visionRange);
-    } else { // Enemy moving left
+    } else {
         ctx.lineTo(enemy.x + enemy.width / 2 - enemy.visionRange, enemy.y - enemy.visionRange);
         ctx.lineTo(enemy.x + enemy.width / 2 - enemy.visionRange, enemy.y + enemy.height + enemy.visionRange);
     }
-
     ctx.closePath();
     ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
     ctx.fill();
 }
 
+let gameRunning = true;
+
+function showGameOverMenu() {
+    document.getElementById("gameOverMenu").classList.add("show");
+    document.getElementById("gameCanvas").style.display = 'none';
+    gameRunning = false;
+}
+
+function restartLevel() {
+    document.getElementById("gameOverMenu").classList.remove("show");
+    document.getElementById("gameCanvas").style.display = 'block';
+    resetGameState(currentLevel);
+    gameRunning = true;
+    cancelAnimationFrame(animationFrameId); // Cancel any existing game loop
+    update();
+}
+
+function showMainMenu() {
+    document.getElementById("mainMenu").classList.add("show");
+    document.getElementById("gameOverMenu").classList.remove("show");
+    document.getElementById("gameCanvas").style.display = 'none';
+    gameRunning = false;
+}
+
+
+
+function updateEnemy(enemy) {
+    enemy.x += enemy.velocityX;
+    if (enemy.x > enemy.endX || enemy.x < enemy.startX) {
+        enemy.velocityX *= -1;
+    }
+    drawEnemy(enemy);
+    if (player.x > enemy.x && player.x < enemy.x + enemy.visionRange && 
+        player.y > enemy.y - enemy.visionRange && player.y < enemy.y + enemy.height + enemy.visionRange) {
+        showGameOverMenu();
+        console.log("Player Detected! Game Over!");
+    }
+}
+
+function resetGameState(level) {
+    // Clear existing game state
+    platforms = [];
+    collectibles = [];
+    enemies = [];
+
+    // General player setup for all levels
+    player.x = 100;
+    player.y = 500;
+    player.velocityX = 0;
+    player.velocityY = 0;
+    player.jumping = false;
+
+    // Level-specific setup
+    if (level === 1) {
+        // Level 1 setup (original setup)
+        platforms.push(
+            { x: 0, y: 550, width: canvas.width, height: 20, color: 'saddlebrown' },
+            { x: 0, y: 350, width: 500, height: 20, color: 'grey' },
+            { x: 800, y: 350, width: 400, height: 20, color: 'grey' },
+            { x: 0, y: 150, width: 400, height: 20, color: 'grey' },
+            { x: 900, y: 150, width: 300, height: 20, color: 'grey' },
+            { x: 550, y: 450, width: 200, height: 20, color: 'peru' },
+            { x: 400, y: 250, width: 200, height: 20, color: 'peru' },
+            { x: 700, y: 250, width: 200, height: 20, color: 'peru' },
+            // Add other platforms as needed
+        );
+        collectibles.push({ x: 150, y: 320, width: 10, height: 10, color: 'gold', collected: false });
+        enemies.push({ x: 200, y: 520, width: 30, height: 30, color: 'red', velocityX: 1, visionRange: 100, startX: 0, endX: canvas.width },
+                     { x: 300, y: 320, width: 30, height: 30, color: 'red', velocityX: 1, visionRange: 100, startX: 0, endX: 500 });
+    } else if (level === 2) {
+        // Level 2 setup
+        platforms.push(
+            // More and smaller platforms
+            { x: 100, y: 500, width: 300, height: 20, color: 'grey' },
+            { x: 500, y: 400, width: 200, height: 20, color: 'grey' },
+            { x: 800, y: 300, width: 150, height: 20, color: 'grey' },
+            // Moving platform
+            { x: 200, y: 200, width: 100, height: 20, color: 'peru', isMoving: true, moveDirection: 'horizontal', moveRange: [200, 400] }
+            // ... Add more as needed
+        );
+        collectibles.push(
+            // Collectibles in challenging locations
+            { x: 350, y: 380, width: 10, height: 10, color: 'silver', collected: false },
+            { x: 850, y: 280, width: 10, height: 10, color: 'silver', collected: false }
+            // ... Add more as needed
+        );
+        enemies.push(
+            // Faster and more enemies
+            { x: 100, y: 480, width: 30, height: 30, color: 'green', velocityX: 1.5, visionRange: 150, startX: 100, endX: 400 },
+            // ... Add more as needed
+        );
+    } else if (level === 3) {
+        // Level 3 setup
+        platforms.push(
+            // Complex platform arrangements
+            { x: 50, y: 450, width: 250, height: 20, color: 'brown' },
+            { x: 400, y: 350, width: 100, height: 20, color: 'brown' },
+            // Disappearing platform
+            { x: 600, y: 250, width: 120, height: 20, color: 'peru', isDisappearing: true, disappearTime: 3000 }
+            // ... Add more as needed
+        );
+        collectibles.push(
+            // Hard-to-reach collectibles
+            { x: 450, y: 330, width: 10, height: 10, color: 'bronze', collected: false },
+            { x: 650, y: 230, width: 10, height: 10, color: 'bronze', collected: false }
+            // ... Add more as needed
+        );
+        enemies.push(
+            // Challenging enemies
+            { x: 60, y: 430, width: 30, height: 30, color: 'purple', velocityX: 2, visionRange: 200, startX: 50, endX: 300 },
+            // ... Add more as needed
+        );
+    }
+}
+
+function startLevel(level) {
+    currentLevel = level;
+    document.getElementById("mainMenu").classList.remove("show");
+    document.getElementById("gameCanvas").style.display = 'block';
+    resetGameState(level); // Initialize game state for the level
+    if (!gameRunning) {
+        gameRunning = true;
+        update(); // Start the game loop only if it's not already running
+    }
+}
+
 
 function update() {
-    // Clear the canvas
+    if (!gameRunning) return;
+
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Gravity and friction
     player.velocityY += gravity;
     player.velocityX *= friction;
 
-    // Process left and right movement
     if (keys.right.pressed && !keys.left.pressed) {
         player.velocityX = player.speed;
     } else if (keys.left.pressed && !keys.right.pressed) {
@@ -105,114 +203,86 @@ function update() {
         player.velocityX = 0;
     }
 
-    // Calculate the next position
     let nextX = player.x + player.velocityX;
     let nextY = player.y + player.velocityY;
 
-    // Assume player is falling until collision detected
     player.jumping = true;
 
-    // Check for collision before updating player's position
+    // Platform collision logic
     platforms.forEach(platform => {
-        // Vertical collision from the top
-        if (
-            nextY + player.height > platform.y && 
+        // Collision from the top
+        if (nextY + player.height > platform.y && 
             player.y + player.height <= platform.y &&
             nextX + player.width > platform.x && 
-            nextX < platform.x + platform.width
-        ) {
+            nextX < platform.x + platform.width) {
             player.velocityY = 0;
             player.jumping = false;
             nextY = platform.y - player.height;
         }
 
-        // Vertical collision from the bottom
-        if (
-            nextY < platform.y + platform.height && 
+        // Collision from the bottom
+        if (nextY < platform.y + platform.height && 
             player.y >= platform.y + platform.height &&
             nextX + player.width > platform.x && 
-            nextX < platform.x + platform.width
-        ) {
+            nextX < platform.x + platform.width) {
             player.velocityY = 0;
-            nextY = player.y; // Keep the player at the current position
+            nextY = player.y;
         }
 
         // Horizontal collision
-        if (
-            nextY + player.height > platform.y && 
-            nextY < platform.y + platform.height
-        ) {
-            if (
-                nextX + player.width > platform.x && 
-                player.x + player.width <= platform.x
-            ) {
+        if (nextY + player.height > platform.y && 
+            nextY < platform.y + platform.height) {
+            if (nextX + player.width > platform.x && 
+                player.x + player.width <= platform.x) {
                 player.velocityX = 0;
                 nextX = platform.x - player.width;
             }
-            if (
-                nextX < platform.x + platform.width && 
-                player.x >= platform.x + platform.width
-            ) {
+            if (nextX < platform.x + platform.width && 
+                player.x >= platform.x + platform.width) {
                 player.velocityX = 0;
                 nextX = platform.x + platform.width;
             }
         }
     });
 
-    // Check for collectible collection
+    // Collectible collision logic
     collectibles.forEach(collectible => {
         if (!collectible.collected &&
-            player.x < collectible.x + collectible.width &&
-            player.x + player.width > collectible.x &&
-            player.y < collectible.y + collectible.height &&
-            player.y + player.height > collectible.y) {
-                collectible.collected = true;
-                // Add score or trigger event
+            nextX < collectible.x + collectible.width &&
+            nextX + player.width > collectible.x &&
+            nextY < collectible.y + collectible.height &&
+            nextY + player.height > collectible.y) {
+            collectible.collected = true;
+            // Add score or trigger event
         }
     });
 
-    // Update and draw enemies
-    enemies.forEach(enemy => {
-        enemy.x += enemy.velocityX;
-        // Change direction if enemy hits a wall
-        if (enemy.x + enemy.width > canvas.width || enemy.x < 0) {
-            enemy.velocityX *= -1;
-        }
+    enemies.forEach(updateEnemy);
 
-        // Check for player detection
-        if (player.x > enemy.x && player.x < enemy.x + enemy.visionRange && 
-            player.y > enemy.y - enemy.visionRange && player.y < enemy.y + enemy.height + enemy.visionRange) {
-            // Player detected
-            console.log("Player Detected! Game Over!");
-            // Implement game over logic
-        }
-
-        drawEnemy(enemy);
-    });
-
-    // Update player's position
     player.x = nextX;
     player.y = nextY;
 
-    // Draw elements
     drawPlayer();
     platforms.forEach(drawPlatform);
     collectibles.forEach(drawCollectible);
 
-    requestAnimationFrame(update);
+
+
+    
+
+    animationFrameId = requestAnimationFrame(update);
 }
 
+// ... [Rest of your existing code]
+
 const keys = {
-    right: {
-        pressed: false
-    },
-    left: {
-        pressed: false
-    },
-    up: {
-        pressed: false
-    }
+    right: { pressed: false },
+    left: { pressed: false },
+    up: { pressed: false }
 };
+
+// Event listeners...
+
 
 window.addEventListener('keydown', (event) => {
     switch (event.key) {
@@ -231,7 +301,7 @@ window.addEventListener('keydown', (event) => {
         case 'W':
         case ' ':
             if (!player.jumping && !keys.up.pressed) {
-                player.velocityY = -10;
+                player.velocityY = -player.jumpStrength;
                 player.jumping = true;
                 keys.up.pressed = true;
             }
